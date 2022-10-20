2a8ed72db9ead9ba795a3640fb227cbe93878d83
joao vitor correa <joaov1997@hotmail.com>
2022-10-20 16:55:32 -0300

    .

--- front/src/components/SectionBackground/SectionContainer.test.jsx
+++ front/src/components/SectionBackground/SectionContainer.test.jsx
@@ -1,10 +1,25 @@
 import { screen } from '@testing-library/react';
 import { renderTheme } from '../../styles/render-theme';
-import { SectionContainer } from '.';
+import { SectionBackground } from '.';
 
-describe('<SectionContainer />', () => {
-  it('should render', () => {
-    renderTheme(<SectionContainer>Children</SectionContainer>);
+describe('<SectionBackground />', () => {
+  it('should render with background dark', () => {
+    const { container } = renderTheme(
+      <SectionBackground background={true}>
+        <h1>Children</h1>
+      </SectionBackground>,
+    );
     expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render with background light', () => {
+    const { container } = renderTheme(
+      <SectionBackground>
+        <h1>Children</h1>
+      </SectionBackground>,
+    );
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 });
