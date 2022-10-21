1c027153c28961bbdf03e10a27fd1c21297d2ffc
joao vitor correa <joaov1997@hotmail.com>
2022-10-21 17:11:18 -0300

    .

--- front/src/components/GridText/styles.js
+++ front/src/components/GridText/styles.js
@@ -1,7 +1,6 @@
 import styled, { css } from 'styled-components';
 import { Title as HeadingContainer } from '../Heading/styles';
 import { Container as TextComponent } from '../TextComponent/styles';
-
 export const Container = styled.div`
   ${({ theme }) => css`
     ${TextComponent} {
@@ -9,13 +8,17 @@ export const Container = styled.div`
     }
   `}
 `;
-
 export const Grid = styled.div`
   ${({ theme }) => css`
     counter-reset: grid-counter;
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
     gap: ${theme.spacings.large};
+    overflow: hidden;
+    width: 100%;
+    @media ${theme.media.lteMedium} {
+      grid-template-columns: 1fr;
+    }
   `}
 `;
 
