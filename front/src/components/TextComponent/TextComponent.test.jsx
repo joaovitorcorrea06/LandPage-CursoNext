64e2446cc94473367ebeca659e48b4445ce4008f
joao vitor correa <joaov1997@hotmail.com>
2022-10-21 15:15:28 -0300

    .

--- front/src/components/TextComponent/TextComponent.test.jsx
+++ front/src/components/TextComponent/TextComponent.test.jsx
@@ -1,24 +1,22 @@
 import { screen } from '@testing-library/react';
-import { TextComponent } from '.';
 import { renderTheme } from '../../styles/render-theme';
-
+import { TextComponent } from '.';
 describe('<TextComponent />', () => {
   it('should render a text', () => {
     renderTheme(<TextComponent>Children</TextComponent>);
     expect(screen.getByText('Children')).toBeInTheDocument();
   });
-
-  // it('should match snapshot', () => {
-  //   const { container } = renderTheme(<TextComponent>Children</TextComponent>);
-  //   expect(container.firstChild).toMatchInlineSnapshot(
-  //     `.c0 {
-  //       font-size: 2.4rem;
-  //     }
-  //     <p
-  //       class="c0"
-  //     >
-  //       Children
-  //     </p>`,
-  //   );
-  // });
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<TextComponent>Children</TextComponent>);
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        font-size: 2.4rem;
+      }
+      <div
+        class="c0"
+      >
+        Children
+      </div>
+    `);
+  });
 });
