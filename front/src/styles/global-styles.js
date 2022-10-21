52e41aa0b912a8f09927d75f3cd9f713109230d7
joao vitor correa <joaov1997@hotmail.com>
2022-10-21 10:27:23 -0300

    .

--- front/src/styles/global-styles.js
+++ front/src/styles/global-styles.js
@@ -1,23 +1,29 @@
-import { createGlobalStyle } from 'styled-components';
-
+import { createGlobalStyle, css } from 'styled-components';
 export const GlobalStyles = createGlobalStyle`
-
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-html {
-  font-size: 62.5%
-}
-
-body{
-  font-size: 2.4rem;
-  font-family: ${({ theme }) => theme.font.family.default};
-}
-
-h1,h2,h3,h4,h5,h6 {
-  font-family: ${({ theme }) => theme.font.family.secondary}
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    font-size: 1.6rem;
+    font-family: ${({ theme }) => theme.font.family.default};
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family: ${({ theme }) => theme.font.family.secondary};
+    margin: ${({ theme }) => theme.spacings.large} 0;
+  }
+  p {
+    margin: ${({ theme }) => theme.spacings.medium} 0;
+  }
+  ul, ol {
+    margin: ${({ theme }) => theme.spacings.medium};
+    padding: ${({ theme }) => theme.spacings.medium};
+  }
+  a {
+    color: ${({ theme }) => theme.colors.secondaryColor};
+  }
 `;
